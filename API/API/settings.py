f634d50ef0eea3ea72bf078b436338bbdc092271
squella12 <esquella@alumnos.uai.cl>
2023-01-23 10:44:37 -0300

    'updated'

--- API/API/settings.py
+++ API/API/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^(4d0wh6o%l_i4pj=b4+wf$0cs2-6=*bg2_h_g!c($!_!^cg)p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  True
